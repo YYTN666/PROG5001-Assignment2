08d1d6658bca8f18aa3e567d4860526ecfe47560
YYTN666 <yang93933@gmail.com>
2023-09-29 14:50:17 +1000

    Commencement of task 1

--- 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
+++ 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
@@ -5,7 +5,18 @@
  * @author (Yutaien Yang)
  * @version (20230929)
  */
+import java.util.Scanner;
+import java.io.*;
+
 public class StudentMarkSystem
 {
-    
+    public static void main (String[] args) {
+        //Prevents errors if a file cannot be found
+        try (Scanner scanner = new Scanner(new File("prog5001_students_grade_2022.csv"))) {
+            
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + "prog5001_students_grade_2022.csv");
+        } 
+    }
 }
