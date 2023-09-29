cb38bfd461bb9beb3f8d3b53e84a89f870752357
YYTN666 <yang93933@gmail.com>
2023-09-29 15:10:00 +1000

    Store the value of the given file in arrays

--- 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
+++ 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
@@ -12,8 +12,13 @@ public class StudentMarkSystem
 {
     public static void main (String[] args) {
         //Prevents errors if a file cannot be found
-        try (Scanner scanner = new Scanner(new File("prog5001_students_grade_2022.csv"))) {
-            
+        try (Scanner scanner = new Scanner(new File("prog5001_students_grade_2022.csv"))) {//read the file
+            int lineCount = 0; //The valid data for the file starts on the third line, so set a counter
+            while (scanner.hasNextLine()) {//Read line by line until there are no more lines
+                String input = scanner.nextLine();//Store the content of the file
+                String[] parts = input.split(",");//Break the content into pieces 
+                
+            }
         }
         catch (FileNotFoundException e) {
             System.out.println("File not found: " + "prog5001_students_grade_2022.csv");
