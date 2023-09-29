dd76306cc13b22f3eb203d8e4f03a68afe8027d7
YYTN666 <yang93933@gmail.com>
2023-09-29 16:45:35 +1000

    Work on task1

--- 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
+++ 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
@@ -18,6 +18,49 @@ public class StudentMarkSystem
                 String input = scanner.nextLine();//Store the content of the file
                 String[] parts = input.split(",");//Break the content into pieces 
                 
+                if (lineCount == 0) {//Read the fist line and store the unit name
+                    String unitName = parts[0] ;
+                    System.out.println(unitName);
+                    System.out.println();
+                }
+                
+                lineCount++;
+                
+                if (lineCount < 3) {//Skip the first two lines
+                    continue;
+                }
+                
+                String studentName = parts[0] + " " + parts[1];
+                String studentID = parts[2];
+                double assignment1;
+                double assignment2;
+                double assignment3;
+                double averageMark;
+                
+                
+                if (parts[3].isEmpty()) {//If the position is empty, set it to 0 points
+                    assignment1 = 0.0;
+                } else {
+                    assignment1 = Double.parseDouble(parts[3]);
+                }
+                if (parts[4].isEmpty()) {//If the position is empty, set it to 0 points
+                    assignment2 = 0.0;
+                } else {
+                    assignment2 = Double.parseDouble(parts[4]);
+                }
+                if (parts[5].isEmpty()) {//If the position is empty, set it to 0 points
+                    assignment3 = 0.0;
+                } else {
+                    assignment3 = Double.parseDouble(parts[5]);
+                }
+                
+                System.out.println("Student Name: " + studentName);
+                System.out.println("Student ID: " + studentID);
+                System.out.print("Assignment1: " + assignment1 + "  ");
+                System.out.print("Assignment2: " + assignment2 + "  ");
+                System.out.print("Assignment3: " + assignment3);
+                System.out.println(); 
+                
             }
         }
         catch (FileNotFoundException e) {
