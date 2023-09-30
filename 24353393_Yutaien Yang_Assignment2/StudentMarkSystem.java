9f33a5853d4cc98de9912d87bcc316fe390cc4c4
YYTN666 <yang93933@gmail.com>
2023-09-30 16:34:10 +1000

    Near completion of tasks I and 2

--- 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
+++ 24353393_Yutaien Yang_Assignment2/StudentMarkSystem.java
@@ -15,51 +15,62 @@ public class StudentMarkSystem
         try (Scanner scanner = new Scanner(new File("prog5001_students_grade_2022.csv"))) {//read the file
             int lineCount = 0; //The valid data for the file starts on the third line, so set a counter
             while (scanner.hasNextLine()) {//Read line by line until there are no more lines
+                lineCount++;
                 String input = scanner.nextLine();//Store the content of the file
                 String[] parts = input.split(",");//Break the content into pieces 
                 
-                if (lineCount == 0) {//Read the fist line and store the unit name
+                if (lineCount == 1) {//Read the fist line and store the unit name
                     String unitName = parts[0] ;
-                    System.out.println(unitName);
+                    System.out.println("Name of unit: " + unitName);
                     System.out.println();
-                }
+                    
+                } else if (lineCount >= 3) {//Skip the first two lines
+                    String studentName = parts[0] + " " + parts[1];
+                    String studentID = parts[2];
+                    double assignment1;
+                    double assignment2;
+                    double assignment3; 
+                    
+                    if (parts.length >= 4) {
+                        if (parts[3].isEmpty()) {
+                            assignment1 = 0.0;
+                        } else {
+                            assignment1 = Double.parseDouble(parts[3]);
+                        }
+                    } else {
+                        assignment1 = 0.0;
+                    }
+                    
+                    if (parts.length >= 5) {
+                        if (parts[4].isEmpty()) {
+                            assignment2 = 0.0;
+                        } else {
+                            assignment2 = Double.parseDouble(parts[4]);
+                        }
+                    } else {
+                        assignment2 = 0.0;
+                    }
+                    
+                    if (parts.length >= 6) {
+                        if (parts[5].isEmpty()) {
+                            assignment3 = 0.0;
+                        } else {
+                            assignment3 = Double.parseDouble(parts[5]);
+                        }
+                    } else {
+                        assignment3 = 0.0;
+                    }
+    
+                    double totalMark = assignment1 + assignment2 + assignment3;
+                    System.out.println("Student " + (lineCount - 2) + " Name: " + studentName);
+                    System.out.println("Student " + (lineCount - 2) + " ID: " + studentID);
+                    System.out.print("Assignment1: " + assignment1 + "  ");
+                    System.out.print("Assignment2: " + assignment2+ "  ");
+                    System.out.println("Assignment3: " + assignment3);
+                    System.out.println("Totals: " + totalMark);
+                    System.out.println();
                 
-                lineCount++;
-                
-                if (lineCount < 3) {//Skip the first two lines
-                    continue;
-                }
-                
-                String studentName = parts[0] + " " + parts[1];
-                String studentID = parts[2];
-                double assignment1;
-                double assignment2;
-                double assignment3;
-                double averageMark;
-                
-                
-                if (parts[3].isEmpty()) {//If the position is empty, set it to 0 points
-                    assignment1 = 0.0;
-                } else {
-                    assignment1 = Double.parseDouble(parts[3]);
-                }
-                if (parts[4].isEmpty()) {//If the position is empty, set it to 0 points
-                    assignment2 = 0.0;
-                } else {
-                    assignment2 = Double.parseDouble(parts[4]);
-                }
-                if (parts[5].isEmpty()) {//If the position is empty, set it to 0 points
-                    assignment3 = 0.0;
-                } else {
-                    assignment3 = Double.parseDouble(parts[5]);
-                }
-                
-                System.out.println("Student Name: " + studentName);
-                System.out.println("Student ID: " + studentID);
-                System.out.print("Assignment1: " + assignment1 + "  ");
-                System.out.print("Assignment2: " + assignment2 + "  ");
-                System.out.print("Assignment3: " + assignment3);
-                System.out.println(); 
+                    }
                 
             }
         }
